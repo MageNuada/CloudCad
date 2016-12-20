c4a6d65c3af36b61d4214ad3fafb7804e040ac24
Nuada <magedmi@land.ru>
2016-12-20 23:19:14 +0300

    Переименование папки.

--- CADView/LogoWindow.xaml.cs
+++ CADView/LogoWindow.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Windows;
 using CADCore;
 using Timer = System.Timers.Timer;
